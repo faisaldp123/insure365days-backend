d3bd7f3d7d42d3580acaceb078ffa6227b2415a5
faisaldp123 <faisal.dpathshala@gmail.com>
2026-05-27 12:15:10 +0530

    made delete single and delete all api

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -7,15 +7,59 @@ const {
   getLeads,
   assignLeads,
   updateLead,
+  deleteLead,
+  deleteAllLeads,
 } = require("../controllers/leadController");
 
-const { protect, adminOnly } = require("../middleware/authMiddleware");
+const {
+  protect,
+  adminOnly,
+} = require("../middleware/authMiddleware");
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+  dest: "uploads/",
+});
 
-router.post("/upload", protect, adminOnly, upload.single("file"), uploadLeads);
-router.get("/", protect, getLeads);
-router.post("/assign", protect, adminOnly, assignLeads);
-router.put("/:id", protect, updateLead);
+router.post(
+  "/upload",
+  protect,
+  adminOnly,
+  upload.single("file"),
+  uploadLeads
+);
+
+router.get(
+  "/",
+  protect,
+  getLeads
+);
+
+router.post(
+  "/assign",
+  protect,
+  adminOnly,
+  assignLeads
+);
+
+// IMPORTANT: keep this before "/:id"
+router.delete(
+  "/delete-all",
+  protect,
+  adminOnly,
+  deleteAllLeads
+);
+
+router.delete(
+  "/:id",
+  protect,
+  adminOnly,
+  deleteLead
+);
+
+router.put(
+  "/:id",
+  protect,
+  updateLead
+);
 
 module.exports = router;
